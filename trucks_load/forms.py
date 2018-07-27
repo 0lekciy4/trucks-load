056b097e17f9a5ca19a7d37bb7a5fc0d4e154f1c
Olekciy <0lekciy4@gmail.com>
2018-07-27 13:46:35 +0300

    Edited forms.py Fixed error with migrating db

--- trucks_load/forms.py
+++ trucks_load/forms.py
@@ -6,13 +6,16 @@ ALL_MODELS = 'Все модели'
 
 
 class TruckTypeForm(forms.Form):
-    trucks_titles_list = [(ALL_MODELS, ALL_MODELS)]
-    for truck in TruckModel.objects.all():
-        trucks_titles_list.append(
-            (truck.title, truck.title,)
-        )
     type_field = forms.ChoiceField(
-        choices=trucks_titles_list,
         initial=ALL_MODELS,
         label='Отсортировать по модели'
     )
+
+    def __init__(self, *args, **kwargs):
+        super(TruckTypeForm, self).__init__(*args, **kwargs)
+        trucks_choices = [(ALL_MODELS, ALL_MODELS)]
+        for truck in TruckModel.objects.all():
+            trucks_choices.append(
+                (truck.title, truck.title)
+            )
+        self.fields['type_field'].choices = trucks_choices
